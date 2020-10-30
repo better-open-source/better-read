5b2b64b224a612e2fc2b43f8f797cbfdef9e1ca9
Alex K <a.katrynets@gmail.com>
2020-10-30 09:36:18 +0200

    wip

---
+++ src/BetterRead/Domain/Errors.fs
@@ -0,0 +1,19 @@
+﻿module BetterRead.Domain.Errors
+
+type DomainError =
+    | ErrorOne
+
+type ValidationError =
+    | ErrorOne
+
+type AppError =
+    | Domain        of DomainError
+    | Validation    of ValidationError
+    static member create (e: DomainError) = Domain e
+    static member create (e: ValidationError) = Validation e
+    static member createResult (e: ValidationError) = Error(Validation e)
+    static member createResult (e: DomainError) = Error(Domain e)
+    static member createResult (e: AppError) =
+        match e with
+        | Domain e        -> AppError.createResult(e)
+        | Validation e    -> AppError.createResult(e)
