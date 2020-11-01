c96e18163777d5d46fae065434e7cff8bc190973
Alex K <a.katrynets@gmail.com>
2020-11-01 23:05:37 +0200

    add encoding for console output

--- src/BetterRead/Program.fs
+++ src/BetterRead/Program.fs
@@ -1,4 +1,6 @@
-﻿open System.Text
+﻿open System
+open System.Diagnostics
+open System.Text
 open BetterRead.Infra
 open HtmlAgilityPack
 
@@ -11,13 +13,20 @@ let htmlWebFactory encoding =
 [<EntryPoint>]
 let main argv =
     Encoding.RegisterProvider CodePagesEncodingProvider.Instance
+    Console.OutputEncoding <- Encoding.UTF8
     
     let bookId = 81173
     let htmlWeb = htmlWebFactory <| encodingBuilder "windows-1251"
+    
+    let timer = Stopwatch()
+    timer.Start()
     
     let bookInfo = BookInfoParser.parse htmlWeb bookId |> Async.RunSynchronously
     bookInfo |> printfn "%A"
     
     let bookPages = BookSheetsParser.parse htmlWeb bookId |> Async.RunSynchronously
     bookPages |> printfn "%A"
+    
+    timer.Stop()
+    printf "sec: %d, ms: %d" timer.Elapsed.Seconds timer.Elapsed.Milliseconds
     0
