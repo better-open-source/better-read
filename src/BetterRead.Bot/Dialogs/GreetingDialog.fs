df66b7d4b240c56d15b18ff65dfe70ae3702a171
Alex K <a.katrynets@gmail.com>
2020-11-23 23:16:14 +0200

    move files to bot project

--- src/BetterRead.Bot/Dialogs/GreetingDialog.fs
+++ src/BetterRead.Bot/Dialogs/GreetingDialog.fs
@@ -4,20 +4,25 @@ open System.Threading
 open BetterRead.Bot.StateAccessors
 open Microsoft.Bot.Builder.Dialogs
 
-type GreetingDialog(dialogId:string, accessors: BotStateAccessors) as this =
-    inherit ComponentDialog(dialogId)
-
+module GreetingDialogModule =
+    
+    [<Literal>]
+    let mainFlowId = "GreetingDialog.mainFlow"
+    
     let finalStepAsync (stepContext: WaterfallStepContext) (cancellationToken: CancellationToken) =
         async {
             let! _= stepContext.Context.SendActivityAsync("Hi from F#!") |> Async.AwaitTask
             return! stepContext.EndDialogAsync(null, cancellationToken) |> Async.AwaitTask
         } |> Async.StartAsTask
+
+    let waterfallSteps = [|
+        WaterfallStep(finalStepAsync)
+    |]
     
+open GreetingDialogModule
+
+type GreetingDialog(dialogId:string, accessors: BotStateAccessors) as this =
+    inherit ComponentDialog(dialogId)
     do
-        let waterfallSteps = [|
-            WaterfallStep(finalStepAsync)
-        |]
-        
-        this.AddDialog(WaterfallDialog("GreetingDialog.mainFlow", waterfallSteps)) |> ignore
-        
-        this.InitialDialogId <- "GreetingDialog.mainFlow"
+        this.AddDialog(WaterfallDialog(mainFlowId, waterfallSteps)) |> ignore
+        this.InitialDialogId <- mainFlowId
--- src/BetterRead.Bot/Dialogs/MainDialog.fs
+++ src/BetterRead.Bot/Dialogs/MainDialog.fs
@@ -4,26 +4,36 @@ open System.Threading
 open BetterRead.Bot.StateAccessors
 open Microsoft.Bot.Builder.Dialogs
 
-type MainDialog(accessors: BotStateAccessors) as this =
-    inherit ComponentDialog()
+module MainDialogModule =
+
+    [<Literal>]
+    let mainFlowId = "MainDialog.mainFlow"
+
+    [<Literal>]
+    let greetingId = "MainDialog.greeting"
 
     let initialStepAsync (stepContext: WaterfallStepContext) (cancellationToken: CancellationToken) =
         async {
-            return! stepContext.BeginDialogAsync("MainDialog.greeting", null, cancellationToken) |> Async.AwaitTask
+            match stepContext.Context.Activity.Text with
+            | _ -> failwith ""
+            return! stepContext.BeginDialogAsync(greetingId, null, cancellationToken) |> Async.AwaitTask
         } |> Async.StartAsTask
     
     let finalStepAsync (stepContext: WaterfallStepContext) (cancellationToken: CancellationToken) =
         async {
             return! stepContext.EndDialogAsync(null, cancellationToken) |> Async.AwaitTask
         } |> Async.StartAsTask
+
+    let waterfallSteps = [|
+        WaterfallStep(initialStepAsync)
+        WaterfallStep(finalStepAsync)
+    |]
     
+open MainDialogModule
+
+type MainDialog(accessors: BotStateAccessors) as this =
+    inherit ComponentDialog()
     do
-        let waterfallSteps = [|
-            WaterfallStep(initialStepAsync)
-            WaterfallStep(finalStepAsync)
-        |]
-        
-        this.AddDialog(GreetingDialog("MainDialog.greeting", accessors)) |> ignore
-        this.AddDialog(WaterfallDialog("MainDialog.mainFlow", waterfallSteps)) |> ignore
-        
-        this.InitialDialogId <- "MainDialog.mainFlow"
+        this.AddDialog(GreetingDialog(greetingId, accessors)) |> ignore
+        this.AddDialog(WaterfallDialog(mainFlowId, waterfallSteps)) |> ignore
+        this.InitialDialogId <- mainFlowId
