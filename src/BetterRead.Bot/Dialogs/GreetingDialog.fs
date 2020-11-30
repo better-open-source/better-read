05186d8223a3138889ba87e9c30ea0cb9874cf98
Alex K <a.katrynets@gmail.com>
2020-11-30 22:18:24 +0200

    update greetings text

--- src/BetterRead.Bot/Dialogs/GreetingDialog.fs
+++ src/BetterRead.Bot/Dialogs/GreetingDialog.fs
@@ -1,8 +1,13 @@
 ﻿namespace BetterRead.Bot.Dialogs
 
+open System.Reflection
+
+open Microsoft.Bot.Builder
+open Microsoft.Bot.Builder.Dialogs
+open Microsoft.Bot.Schema
+
 open BetterRead.Common.AsyncExtensions
 open BetterRead.Bot.StateAccessors
-open Microsoft.Bot.Builder.Dialogs
 
 module GreetingDialogModule =
     
@@ -16,15 +21,24 @@ module private InternalGreetingDialogModule =
     [<Literal>]
     let mainFlowId = "GreetingDialog.mainFlow"
     
+    [<Literal>]
+    let cardImage = "https://github.com/better-open-source/better-read/raw/main/assets/icon.png"
+    
     let finalStepAsync (stepContext : WaterfallStepContext) cancellationToken =
         async {
-            do! stepContext.Context.SendActivityAsync("Hi from F#!") |> Async.AwaitTask |> Async.Ignore
+            let version = Assembly.GetExecutingAssembly().GetName().Version
+            let versionText = version.ToString(3)
+            let title = $"BetterRead bot (v.{versionText})"
+            let text = "Greetings! Gonna read books offline? LoveRead books parser and document generator tool is ready to help!"
+            let image = ResizeArray<CardImage> [ CardImage(cardImage) ]
+            let card = HeroCard(title = title, text = text, images = image)
+            let activity = MessageFactory.Attachment(card.ToAttachment())
+            
+            do! stepContext.Context.SendActivityAsync(activity) |> Async.AwaitTask |> Async.Ignore
             return! stepContext.EndDialogAsync(null, cancellationToken) |> Async.AwaitTask
         } |> Async.StartAsTask
 
-    let waterfallSteps = [|
-        WaterfallStep(finalStepAsync)
-    |]
+    let waterfallSteps = [| WaterfallStep(finalStepAsync) |]
     
 open InternalGreetingDialogModule
 
