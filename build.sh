94c7318269c2429e77b33a8b9e55284226a41690
Alex K <a.katrynets@gmail.com>
2020-11-30 22:40:18 +0200

    fix build scripts

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 ﻿#!/usr/bin/env bash
 
-docker build . -t build-image -f Dockerfile --build-arg CI_BUILDID=$1 --build-arg CI_PRERELEASE=$2
+docker build . -t build-image -f src/BetterRead.API/Dockerfile --build-arg CI_BUILDID=$1 --build-arg CI_PRERELEASE=$2
 docker create --name build-container build-image
 docker cp build-container:./app/out ./out
 docker rm -fv build-container
