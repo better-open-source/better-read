676c16f857674df83a01df97e78bc85635118e78
Alex <a.katrynets@gmail.com>
2020-12-02 16:11:23 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 docker build . -t build-image -f src/BetterRead.Api/Dockerfile --build-arg CI_BUILDID=$1 --build-arg CI_PRERELEASE=$2
 docker create --name build-container build-image
 docker cp build-container:./app/out ./out
-tree -d 
+ls -R | grep ":$" | sed -e 's/:$//' -e 's/[^-][^\/]*\//--/g' -e 's/^/   /' -e 's/-/|/'
 docker rm -fv build-container
